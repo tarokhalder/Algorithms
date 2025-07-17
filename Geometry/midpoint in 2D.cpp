2c56e7830a19ca13230e4679f297cc85f390a085
tarokhalder <139262330+tarokhalder@users.noreply.github.com>
2025-07-17 16:43:25 +0600

    midpoint in 2D.cpp

---
+++ Geometry/midpoint in 2D.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    int x1 , y1 , x2 , y2;
+    cin >> x1 >> y1 >> x2 >> y2;
+    cout <<(float) (x1 + x2) / 2 << " " << (float)(y1 + y2) / 2 << "\n";
+    return 0;
+}
